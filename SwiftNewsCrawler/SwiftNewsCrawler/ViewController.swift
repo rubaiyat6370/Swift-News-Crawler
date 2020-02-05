2965f3cf79f603e30e0a9daf46e12d0ace91ea24
Rubaiyat Jahan Mumu <rubaiyat.jahan@gmail.com>
2020-02-05 12:50:33 -0500

    Single view application created

---
+++ SwiftNewsCrawler/SwiftNewsCrawler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftNewsCrawler
+//
+//  Created by Rubaiyat Jahan Mumu on 2020-02-05.
+//  Copyright © 2020 Rubaiyat Jahan Mumu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
