3c023172b4baed4687b0f752e4c8d7ed98d029d9
Rubaiyat Jahan Mumu <rubaiyat.jahan@gmail.com>
2020-08-30 09:18:23 -0400

    New target and bitrise.yml file added

---
+++ SwiftNewsCrawler/WhiteLabelTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhiteLabelTest
+//
+//  Created by Rubaiyat Jahan Mumu on 2020-08-30.
+//  Copyright © 2020 Rubaiyat Jahan Mumu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
